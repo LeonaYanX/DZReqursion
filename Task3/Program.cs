8fce337b7cf20a94d63683efa9b4d878e75de329
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-26 16:22:01 +0400

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца.
+//Использовать рекурсию, не использовать циклы.
+
+int[] GetRandomArray()
+{
+    Random random = new Random();
+    int[] array = new int[random.Next(1 ,10)];
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = random.Next(0,101);
+    }
+    return array;
+}
+void PrintArrayVS(int[] array, int size)
+{
+    if (size-1 < 0) 
+    {
+        Console.WriteLine();
+        return;
+    }
+    Console.Write(array[size-1] + " ");
+    PrintArrayVS(array,size-1);
+}
+void PrintArray(int[] array)
+{
+   for (int i = 0;i < array.Length;i++) 
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+
+int[] myArray = GetRandomArray();
+PrintArray(myArray);
+Console.WriteLine();
+PrintArrayVS(myArray, myArray.Length);
+Console.ReadLine();
